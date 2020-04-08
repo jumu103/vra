153f7eed13bf1ddfae8c9ed71752a3ef34b05ed7
jumu103 <jumu103@users.noreply.github.com>
2020-04-08 16:43:39 +0200

    Action Version: 1

---
+++ vra8-ABX/jm test input fields.py
@@ -0,0 +1,10 @@
+def handler(context, inputs):
+    
+    old_name = inputs["old_name"]
+    new_name = inputs["new_name"]
+    
+    print("Got two values {0} --and-- {1}" .format(old_name, new_name))
+    
+    outputs = {}
+
+    return outputs
